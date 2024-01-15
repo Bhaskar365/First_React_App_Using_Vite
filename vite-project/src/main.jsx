16765d2ccfd5cfb93066f11d66c616d44e3bcf32
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-15 14:53:52 +0530

    custom react folder added

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click to get goggle'
+};
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
     <App />
