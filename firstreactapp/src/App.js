e696847e12c793c9ebf96e19644d6dbea21ab4ed
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-15 13:24:59 +0530

    file added for vite and similar changes in firstreactapp folder build on normal react to compare changes

---
+++ firstreactapp/src/App.js
@@ -0,0 +1,12 @@
+import Test from './test.js'
+
+function App() {
+  return (
+    <>
+      <h1>This is react</h1>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ firstreactapp/src/test.js
@@ -0,0 +1,10 @@
+function Test(){
+    return (
+        <>
+            <h1>Create React App h1 tag</h1>
+            <h2>Create React App h2 tag</h2>
+        </>
+    )
+}
+
+export default Test
